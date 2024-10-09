11de33a683aac5c2942a49ce1cd97781cd555520
Sergio <seregaant71@gmail.com>
2024-10-09 21:38:50 +0300

    Декомпозиция, меняющегося поведения fly(), quack()

---
+++ Strategy/SimUDuc/Duck.rb
@@ -0,0 +1,20 @@
+class Duck
+  attr_accessor: fly_behavior, quack_behavior
+  
+  def initialize(fly_behavior, quack_behavior)
+    @fly_behavior = fly_behavior
+    @quack_behavior
+  end
+
+  def perform_fly
+    @fly_behavior.fly
+  end
+
+  def perform_quack
+    @quack_behavior.quck
+  end
+
+  def swim
+    puts "I can swim"
+  end
+end
---
+++ Strategy/SimUDuc/main.rb
@@ -0,0 +1,4 @@
+rubber_duck = Duck.new()
+
+rubber_duck.fly
+rubber_duck.quck
